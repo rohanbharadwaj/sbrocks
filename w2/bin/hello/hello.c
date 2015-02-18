c00f83a38ceeb08bcce939bdce410271e08e8a4e
shashiranjansiso <shashi.ranjan@stonybrook.edu>
2015-02-17 19:33:46 -0500

    merged project2 with andrew

--- w2/bin/hello/hello.c
+++ w2/bin/hello/hello.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char* argv[], char* envp[]) {
-	char c[1024];
-	printf("type some character \n");
-	scanf("%s", c);
-	printf("read char is %s \n", c);
+	printf("Hello World!\n");
 	return 0;
 }
-
-
-
